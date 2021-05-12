e65a61a2658969969d6da7332b1970c7751a2c88
Tim <s181029@students.aie.edu.au>
2021-05-12 13:23:31 +1000

    Cutscene script almost
    Added cutscene script. Could be better, but it will work for now.

---
+++ Assets/Sandbox Files/Timothy/DisplayCutscene.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayCutscene : MonoBehaviour
+{
+    public List<string> messages = new List<string>();
+    private int currentmessage;
+    bool inTrigger = false;
+    public GameObject CutsceneIndicator;
+    [SerializeField]
+    private GameObject cutscenecharacters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentmessage = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inTrigger == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (currentmessage < messages.Count)
+                {
+                    GameManager.MainTextbox.text = messages[currentmessage];
+                    currentmessage++;
+                    GameManager.MainTextBoxImage.gameObject.SetActive(true);
+                    cutscenecharacters.SetActive(true);
+                    CutsceneIndicator.SetActive(false);
+                }
+                else
+                {
+                    GameManager.MainTextbox.text = "";
+                    GameManager.MainTextBoxImage.gameObject.SetActive(false);
+                    currentmessage = 0;
+                    cutscenecharacters.SetActive(false);
+                    CutsceneIndicator.SetActive(true);
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        inTrigger = true;
+        CutsceneIndicator.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        inTrigger = false;
+        CutsceneIndicator.SetActive(false);
+    }
+}
