29ba9f78af45803cf7cbcca06cb1234c133cf5cd
Corey Bloomfield <bloomfieldcorey@gmail.com>
2021-05-13 15:17:38 +1000

    Added 2 prefabs and TMP to testing

---
+++ Assets/Sandbox Files/Corey/LevelChanger.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+
+	public Animator animator;
+
+	private int levelToLoad;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown("space"))
+		{
+			FadeToNextLevel();
+		}
+	}
+
+	public void FadeToNextLevel()
+	{
+		FadeToLevel(SceneManager.GetActiveScene().buildIndex + 0);
+	}
+
+	public void FadeToLevel(int levelIndex)
+	{
+		levelToLoad = levelIndex;
+		animator.SetTrigger("FadeOut");
+	}
+
+	public void OnFadeComplete()
+	{
+		SceneManager.LoadScene(levelToLoad);
+	}
+}
