0c692d46c923c6998aa214a9687ef36f5c2597ba
Tim <s181029@students.aie.edu.au>
2021-05-13 12:33:52 +1000

    Cutscene testing
    Practicing using Timeline to make a better 1st cutscene

---
+++ Assets/Sandbox Files/Timothy/TimelineController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimelineController : MonoBehaviour
+{
+    public List<PlayableDirector> playableDirectors;
+    public List<TimelineAsset> timelines;
+
+    public void Play()
+    {
+        foreach (PlayableDirector playableDirector in playableDirectors)
+        {
+            playableDirector.Play();
+        }
+    }
+    public void PlayFromTimelines(int index)
+    {
+        TimelineAsset selectedAsset;
+        selectedAsset = timelines[index];
+    }
+}
