043c95b30af653c4b4c98a1e5ff3fbf8897a1b39
Tim <s181029@students.aie.edu.au>
2021-05-14 10:29:24 +1000

    Adding in cutscenes
    placed new timeline cutscenes into project. The initial dialogue system goes over it for the time being.

---
+++ Assets/Sandbox Files/Timothy/CutscenesV2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutscenesV2 : MonoBehaviour
+{
+    bool inTrigger = false;
+    public GameObject CutsceneIndicator;
+    public GameObject Actors;
+    [SerializeField]
+    private GameObject CutsceneDirector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CutsceneIndicator.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inTrigger == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                CutsceneDirector.SetActive(true);
+                Actors.SetActive(true);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        inTrigger = true;
+        CutsceneIndicator.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        inTrigger = false;
+        CutsceneIndicator.SetActive(false);
+        CutsceneDirector.SetActive(false);
+        Actors.SetActive(false);
+    }
+}
