0ad0e62a9b7d6a22503aa444818ae99c92e939c3
Tim <s1810>
2021-05-17 11:01:29 +1000

    V3 cutscene tests
    Made an improved cutscene in my testing scene with dialogue that plays automatically and has character icons

---
+++ Assets/Sandbox Files/Timothy/CutscenesV3.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutscenesV3 : MonoBehaviour
+{
+    bool inTrigger = false;
+    public GameObject CutsceneIndicator;
+    public GameObject Actors;
+    public GameObject Dialogue;
+    [SerializeField]
+    private GameObject CutsceneDirector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CutsceneIndicator.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inTrigger == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                CutsceneDirector.SetActive(true);
+                Actors.SetActive(true);
+                Dialogue.SetActive(true);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        inTrigger = true;
+        CutsceneIndicator.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        inTrigger = false;
+        CutsceneIndicator.SetActive(false);
+        CutsceneDirector.SetActive(false);
+        Actors.SetActive(false);
+        Dialogue.SetActive(false);
+    }
+}
